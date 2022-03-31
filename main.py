50c13997c2601e8b228a1f48d0eab902e7a044e2
Danechka <silnovdanila2@gmail.com>
2022-03-31 13:40:48 +0700

    Добавил Авторизацию

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template
-# from flask_login import LoginManager
+from flask_login import LoginManager, login_user, current_user
 from werkzeug.utils import redirect
 
 from data import db_session
 from data.news import News
 from data.users import User
+from forms.LoginForm import LoginForm
 from forms.user import RegisterForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-"""login_manager = LoginManager()
+login_manager = LoginManager()
 login_manager.init_app(app)
-"""
+app.config['SECRET_KEY'] = 'SILNOVDANYA_secret_key'
 
 
 def check_password(password):
@@ -32,16 +32,21 @@ def main():
     app.run()
 
 
-"""@login_manager.user_loader
+@login_manager.user_loader
 def load_user(user_id):
     db_sess = db_session.create_session()
-    return db_sess.query(User).get(user_id)"""
+    return db_sess.query(User).get(user_id)
 
 
 @app.route("/")
 def index():
     db_sess = db_session.create_session()
     news = db_sess.query(News).filter(News.is_private != True)
+    if current_user.is_authenticated:
+        news = db_sess.query(News).filter(
+            (News.user == current_user) | (News.is_private != True))
+    else:
+        news = db_sess.query(News).filter(News.is_private != True)
     return render_template("ind.html", news=news)
 
 
@@ -73,6 +78,21 @@ def reqister():
         db_sess.commit()
         return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 if __name__ == '__main__':
